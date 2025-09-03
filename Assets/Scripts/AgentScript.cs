d72060b12ffc3ee74775c7fcb0182e813f88fc62
Yuseeeee <49379138@est.ort.edu.ar>
2025-09-03 09:08:18 -0300

    a

---
+++ Assets/Scripts/AgentScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentScript : MonoBehaviour
+{
+    NavMeshAgent agent;
+    [SerializeField] Transform targetTR;
+    [SerializeField] Animator anim;
+    [SerializeField] float velocity;
+    [SerializeField] bool useClickToMove;
+    [SerializeField] Transform[] waypoints;
+    [SerializeField] Vector3 currentDestination;
+    RaycastHit m_HitInfo = new RaycastHit();
+    [SerializeField] int currentWaypointIndex=0;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        currentDestination = waypoints[0].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (!useClickToMove)
+        {
+            //agent.destination = targetTR.position;
+            agent.destination = currentDestination;
+            if(agent.remainingDistance < .5f)
+            {
+                currentWaypointIndex++;
+                if(currentWaypointIndex >= waypoints.Length -1)
+                {
+                    currentWaypointIndex = 0;
+                }
+                currentDestination = waypoints[currentWaypointIndex].position;
+            }
+
+        }
+        velocity = agent.velocity.magnitude;
+        anim.SetFloat("Speed",velocity);
+    }
+
+}
---
+++ Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickToMove : MonoBehaviour
+{
+    NavMeshAgent m_Agent;
+    RaycastHit m_HitInfo = new RaycastHit();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin,ray.direction, out m_HitInfo))
+            {
+                m_Agent.destination = m_HitInfo.point;
+            }
+        }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(Camera.main.transform.position, m_HitInfo.point);
+    }
+}
